380c56c5560ce32a1438d84fe912dc226f5522eb
TomasCedeno <tomasj.cedenop@gmail.com>
2022-08-08 14:23:47 -0500

    build folder added to gitignore

--- build/models/enums.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TypeCourse = void 0;
-var TypeCourse;
-(function (TypeCourse) {
-    TypeCourse["Theo"] = "te\u00F3rico";
-    TypeCourse["TheoPrac"] = "te\u00F3rico-pr\u00E1ctico";
-})(TypeCourse = exports.TypeCourse || (exports.TypeCourse = {}));
