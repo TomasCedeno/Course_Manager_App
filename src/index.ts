d6442a270e907c696294aa2a7b53ff6e9b0cdf93
TomasCedeno <tomasj.cedenop@gmail.com>
2022-08-09 01:17:13 -0500

    solved problem with cors

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 
 import studentsRouter from './routes/students'
 import coursesRouter from './routes/courses'
@@ -8,6 +9,7 @@ const app = express()
 
 //Middlewares
 app.use(express.json())
+app.use(cors())
 
 const PORT = 3000
 
